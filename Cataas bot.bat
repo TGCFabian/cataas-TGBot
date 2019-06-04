c129aa1b0e53bab20e126637523fbd21b12680e8
TGCFabian <37806580+TGCFabian@users.noreply.github.com>
2019-06-04 12:32:27 +0200

    The files!

---
+++ Cataas bot.bat
@@ -0,0 +1,3 @@
+:start
+node ./cataas.js
+goto start
---
+++ cataas.js
@@ -0,0 +1,50 @@
+const TelegaBot = require('telega-bot');
+const bot = new TelegaBot('BOT ID!');
+bot.start();
+
+const API1 = 'https://cataas.com/cat';
+const API2 = 'https://cataas.com/cat/gif';
+
+// Log every text message
+bot.on('text', function (msg) {
+    console.log(`[text] ${ msg.chat.id } ${ msg.text }`);
+});
+
+
+// On command "cataas" or "cataasgif"
+
+bot.on(['/cataas', '/cataasgif'], function (msg) {
+
+    let promise;
+    let id = msg.chat.id;
+    let cmd = msg.text.split(' ')[0];
+
+    // Photo or gif?
+    if (cmd == '/cataas') {
+        promise = bot.sendPhoto(id, API1, {
+            fileName: 'cataas.jpg',
+            serverDownload: true
+        });
+    } else {
+        promise = bot.sendDocument(id, API2, {
+            fileName: 'cataas.gif',
+            serverDownload: true
+        });
+    }
+	
+		
+    // Send "uploading photo" action
+    bot.sendAction(id, 'upload_photo');
+
+    return promise.catch(error => {
+        console.log('[error]', error);
+        // Send an error
+        bot.sendMessage(id, `😿 An error ${ error } occurred, try again.`);
+    });
+
+});
+
+
+bot.on('/start', (msg) => {
+    return msg.reply.text('Hey, Welcome to the Cataas Telegram Bot, Use /cataas or /cataasgif for pics and gifs!', { asReply: true });
+});
